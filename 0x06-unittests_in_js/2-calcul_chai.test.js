0eaee6fd93dda9e483f75177aef4e6c6ff80ac6c
hellena254 <hellenah243@gmail.com>
2024-08-26 07:01:02 -0700

    add the files

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,39 @@
+const { expect } = require('chai');
+const calculateNumber = require('./2-calcul.js');
+
+describe('calculateNumber', function() {
+  describe('SUM', function() {
+    it('should return 6 when inputs are 1.4 and 4.5', function() {
+      expect(calculateNumber('SUM', 1.4, 4.5), 6);
+    });
+
+    it('should return 0 when inputs are -1.4 and 1.4', function() {
+      assert.strictEqual(calculateNumber('SUM', -1.4, 1.4), 0);
+    });
+  });
+
+  describe('SUBTRACT', function() {
+    it('should return -4 when inputs are 1.4 and 4.5', function() {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    });
+
+    it('should return 2 when inputs are 4.6 and 2.1', function() {
+      assert.strictEqual(calculateNumber('SUBTRACT', 4.6, 2.1), 2);
+    });
+  });
+
+  describe('DIVIDE', function() {
+    it('should return 0.2 when inputs are 1.4 and 4.5', function() {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    });
+
+    it('should return Error when divisor rounds to 0', function() {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    });
+
+    it('should return 5 when inputs are 9.6 and 1.8', function() {
+      assert.strictEqual(calculateNumber('DIVIDE', 9.6, 1.8), 5);
+    });
+  });
+});
+
